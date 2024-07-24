b61025c30d7042bb6c73ebe388c7b59a1d40e071
kirinokirino <helloworldagain2@gmail.com>
2024-07-24 21:30:41 +0200

    make width and height adjustable

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use speedy2d::window::{WindowCreationOptions, WindowHandler, WindowHelper, Windo
 use speedy2d::{Graphics2D, Window};
 
 use memmap2::Mmap;
+use std::env;
 use std::fs::File;
 use std::io::Write;
 
@@ -12,6 +13,15 @@ const WIDTH: u16 = 640;
 const HEIGHT: u16 = 480;
 
 fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let (width, height) = if args.len() == 2 {
+        (
+            args[0].parse::<u16>().unwrap_or(WIDTH),
+            args[1].parse::<u16>().unwrap_or(HEIGHT),
+        )
+    } else {
+        (WIDTH, HEIGHT)
+    };
     let mut file = File::options()
         .create(true)
         .truncate(true)
@@ -19,24 +29,30 @@ fn main() {
         .write(true)
         .open("/tmp/imagesink")
         .unwrap();
-    let _ = file.write_all(vec![0; usize::from(WIDTH) * usize::from(HEIGHT) * 4].as_slice());
+    let _ = file.write_all(vec![0; usize::from(width) * usize::from(height) * 4].as_slice());
 
     let mmap = unsafe { Mmap::map(&file).unwrap() };
     let _ = mmap.lock();
 
-    let size = UVec2::new(u32::from(WIDTH), u32::from(HEIGHT));
+    let size = UVec2::new(u32::from(width), u32::from(height));
     let window_size = WindowSize::PhysicalPixels(size);
     let window_options = WindowCreationOptions::new_windowed(window_size, None)
         .with_decorations(false)
         .with_transparent(true)
         .with_resizable(false);
     let window = Window::new_with_options("FLOATING", window_options).unwrap();
-    window.run_loop(MyWindowHandler { mmap });
+    window.run_loop(MyWindowHandler {
+        mmap,
+        width,
+        height,
+    });
 }
 
 struct MyWindowHandler {
     //image: Option<ImageHandle>,
     mmap: Mmap,
+    width: u16,
+    height: u16,
 }
 
 impl WindowHandler for MyWindowHandler {
@@ -45,7 +61,7 @@ impl WindowHandler for MyWindowHandler {
             .create_image_from_raw_pixels(
                 ImageDataType::RGBA,
                 ImageSmoothingMode::NearestNeighbor,
-                UVec2::new(u32::from(WIDTH), u32::from(HEIGHT)),
+                UVec2::new(u32::from(self.width), u32::from(self.height)),
                 &self.mmap[..],
             )
             .unwrap();
